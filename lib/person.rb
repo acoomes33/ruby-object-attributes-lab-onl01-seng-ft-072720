18a1dac2bb3ab199d1f4a832e257bfeeaa0bcb99
Alan Coomes <acoomes1@cord.edu>
2020-08-06 19:41:33 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name=(person_name)
+    @this_person_name = person_name
+  end
+  
+  def name
+    @this_person_name
+  end
+  
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+  
+  def breed
+    @this_dog_breed
+  end
+  
+end
